dc4c556fa406b041d5f969d3dc4fd1967f0b9da9
XPS-9560\Dorian <popa.dorian98@gmail.com>
2020-01-17 02:19:45 +0200

    Extracted Group class to it's own file

---
+++ BioInspiredOptimization/BioInspiredOptimization/Individual/Group.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BioInspiredOptimization.Individual
+{
+    class Group : IComparable<Group>
+    {
+        public int GroupSize;
+        public Agent Head;
+        public Agent Tail;
+
+        public Group(Agent head, int groupSize)
+        {
+            GroupSize = groupSize;
+            Head = head;
+            Tail = new Agent(head.Coords, head.Error);
+        }
+
+        public int CompareTo(Group y)
+        {
+                return Head.CompareTo(y.Head);
+        }
+    }
+}
--- BioInspiredOptimization/BioInspiredOptimization/Solvers/CompetitionOverResources.cs
+++ BioInspiredOptimization/BioInspiredOptimization/Solvers/CompetitionOverResources.cs
@@ -133,24 +133,5 @@ namespace BioInspiredOptimization.Solvers
             
             return bestGlobalPosition;
         }
-
-        class Group : IComparable<Group>
-        {
-            public int GroupSize;
-            public Agent Head;
-            public Agent Tail;
-
-            public Group(Agent head, int groupSize)
-            {
-                GroupSize = groupSize;
-                Head = head;
-                Tail = new Agent(head.Coords, head.Error);
-            }
-
-            public int CompareTo(Group y)
-            {
-                return Head.CompareTo(y.Head);
-            }
-        }
     }
 }
